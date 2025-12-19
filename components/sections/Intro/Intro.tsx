e8884097cfa73f817ab1226a876a910a9cae209f
Leo Jonsson <jonssonleonardo@gmail.com>
2025-12-19 15:40:58 +0100

    Hmmm

--- components/sections/Intro/Intro.tsx
+++ components/sections/Intro/Intro.tsx
@@ -43,7 +43,7 @@ const Intro = () => {
           transition={{ duration: 1, ease: 'easeInOut', delay: 0.1 }}
         />
       </div>
-      <div className="w-full grid lg:grid-cols-2 overflow-hidden relative max-w-[120rem] h-screen font-mono mx-auto py-2">
+      <div className="w-full grid lg:grid-cols-2 overflow-hidden relative max-w-[200rem] h-screen font-mono mx-auto py-2">
         <div className="flex flex-col lg:justify-around justify-center lg:gap:0 gap-8 w-full px-2 ">
           <div className="max-w-[35rem] w-full  max-h-[20rem] top-4 lg:pl-8">
             <TextAnimate
@@ -97,8 +97,8 @@ const Intro = () => {
                   <Image
                     src={`/assets/me/${image.url}`}
                     alt={`image-${idx}`}
-                    height={200}
-                    width={200}
+                    height={700}
+                    width={700}
                     className={`mb-2 rounded-none ${
                       image.isLandscape ? 'aspect-[4/3]' : 'h-auto'
                     } object-cover w-full grayscale-80 hover:grayscale-0 transition-all`}
